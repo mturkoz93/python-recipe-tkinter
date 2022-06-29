a06495b2997c0ea3f5fa2a72b6bd6de962178717
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-29 13:03:42 +0300

    ADD frames

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title('Tkinter Recipe App')
-window.geometry('600x400')
+window.geometry('600x400+10+20') # "widthxheight+XPOS+YPOS"
+
+frame1 = tk.Frame(master=window, width=200, height=100, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame2 = tk.Frame(master=window, width=400, bg="yellow")
+frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+label1 = tk.Label(master=frame2, text="I'm at (0, 0)", bg="blue", fg="white")
+label1.place(x=1, y=0)
 
 window.mainloop()

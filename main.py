201817e7d3d10b51ad2a0a25ae822291b126a166
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-29 20:33:38 +0300

    added frames& C:/Users/mturk/AppData/Local/Programs/Python/Python310/python.exe c:/Users/mturk/Desktop/adessoOkr22H1/recipe-tkinter/main.py

--- main.py
+++ main.py
@@ -1,17 +1,73 @@
 import tkinter as tk
+from PIL import ImageTk
+import requests
 
 
-window = tk.Tk()
-window.title('Tkinter Recipe App')
-window.geometry('600x400+10+20') # "widthxheight+XPOS+YPOS"
+bg_colour = "#3d6466"
 
-frame1 = tk.Frame(master=window, width=200, height=100, bg="red")
-frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+def load_frame1():
+    frame1.pack_propagate(False)
+    # logo widget
+    logo_img = ImageTk.PhotoImage(file="assets/Recipe_logo.png")
+    logo_widget = tk.Label(frame1, image=logo_img, bg=bg_colour)
+    logo_widget.image = logo_img
+    logo_widget.pack()
 
-frame2 = tk.Frame(master=window, width=400, bg="yellow")
-frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-label1 = tk.Label(master=frame2, text="I'm at (0, 0)", bg="blue", fg="white")
-label1.place(x=1, y=0)
+    # instructions widget
+    tk.Label(
+        frame1, 
+        text="ready for your random recipe?",
+        bg=bg_colour,
+        fg="white",
+        font=("TkMenuFont", 14)
+        ).pack()
 
-window.mainloop()
+    # button widget
+    tk.Button(
+        frame1,
+        text="SHUFFLE",
+        font=("TkHeadingFont", 20),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda: load_frame2()
+    ).pack(pady=20)
+
+def load_frame2():
+    print("Hello mustafa")
+
+
+# initiallize app
+root = tk.Tk()
+
+root.title("Tkinter Recipe App")
+# root.eval("tk::PlaceWindow . center")
+x = root.winfo_screenwidth() // 2
+y = int(root.winfo_screenheight() * 0.1)
+root.geometry('500x600+' + str(x) + '+' + str(y))
+# root.geometry('600x400+10+20') # "widthxheight+XPOS+YPOS"
+
+# create a frame widget
+frame1 = tk.Frame(root, width=500, height=600, bg=bg_colour)
+frame2 = tk.Frame(root, bg=bg_colour)
+
+for frame in (frame1, frame2):
+    frame.grid(row=0, column=0)
+
+load_frame1()
+
+
+def getRecipes():
+    url = "https://adesso-recipe-flask.herokuapp.com/recipes"
+    response = requests.get(url)
+    
+    return response.json()
+
+# label1.configure(text=getRecipes()[0]["steps"])
+
+
+# run app
+root.mainloop()
